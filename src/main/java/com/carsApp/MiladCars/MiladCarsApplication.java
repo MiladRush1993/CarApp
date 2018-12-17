e057f4c3bd61c5de99422b96e20549a6419479b7
Rental-Media <Rental-Media@ASUS_2520>
2018-12-17 18:22:35 +0200

    static content add (47)

--- src/main/java/com/carsApp/MiladCars/MiladCarsApplication.java
+++ src/main/java/com/carsApp/MiladCars/MiladCarsApplication.java
@@ -1,14 +1,23 @@
 package com.carsApp.MiladCars;
+import org.slf4j.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class MiladCarsApplication {
 
+	  private static final Logger logger = LoggerFactory.getLogger(MiladCarsApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MiladCarsApplication.class, args);
+		logger.info("Hello Spring Boot");
+	  }
+
+		//rain
 	}
 
-}
+
 
