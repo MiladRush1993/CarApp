59564c96c299d114ac3d8ff954be8823db12105c
Rental-Media <Rental-Media@ASUS_2520>
2018-12-17 17:04:24 +0200

    First Initial commit (Milad)

---
+++ src/main/java/com/carsApp/MiladCars/MiladCarsApplication.java
@@ -0,0 +1,14 @@
+package com.carsApp.MiladCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiladCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiladCarsApplication.class, args);
+	}
+
+}
+
